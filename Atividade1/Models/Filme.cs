526190f57951f284c21e755cd86c5871b1d77112
Espalhabrasas <soespalhabrasas@gmail.com>
2025-02-13 00:40:53 +0000

    Upload model

---
+++ Atividade1/Models/Filme.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace Atividade1.Models
+{
+	public class Filme
+	{
+		public string nome { get; set; }
+	}
+}
